6c7291555104adb4f1f133210232a1ea87468c2b
hhtznr <57875126+hhtznr@users.noreply.github.com>
2020-06-11 09:27:50 +0200

    Add processing of a single video file or video files in a directory
    Call the thumbnails creation method if the specified file appears to be
    a video file by its file extension or if files found in the specified
    directory appear to be video files. Optionally, a directory can be
    processed recursively.

--- pyvideothumbnailer.py
+++ pyvideothumbnailer.py
@@ -16,6 +16,15 @@ VIDEO_EXTENSIONS = ('.avi',
                     '.mpg',
                     '.wmv')
 
+def create_preview_thumbnails(file_path: str) -> None:
+    """
+    Create preview thumbnails of a video file.
+
+    Parameters:
+    file_path (str): The path of the video file of which to create thumbnails.
+    """
+    print('Creating preview thumbnails for \'{}\' ...'.format(os.path.abspath(file_path)))
+
 def has_video_extension(file_name: str) -> bool:
     """
     Checks if a file name ends with a video extension.
@@ -31,9 +40,50 @@ def has_video_extension(file_name: str) -> bool:
     """
     return file_name.lower().endswith(VIDEO_EXTENSIONS)
 
+def process_file_or_directory(path: str, recursive: bool) -> None:
+    """
+    Process a file or directory and create preview thumbnails of identified video files.
+
+    If called on a file, preview thumbnails are created if the file is a video.
+    If called on a directory, preview thumbnails are created of video files found in the directory.
+
+    Parameters:
+    path (str): The absolute or relative path of the file or directory.
+    recursive (bool): If path is a directory and True, process any subdirectories as well.
+    """
+    # List of files and directories to process
+    file_names = None
+    # If the path is a directory, list its contents
+    if os.path.isdir(path):
+        file_names = sorted(os.listdir(path))
+    # If the path is a file, get its dirname and basename
+    elif os.path.isfile(path):
+        path_elements = os.path.split(path)
+        # dirname
+        path = path_elements[0]
+        # basename (as sole element in a list to be compatible with iteration below)
+        file_names = [path_elements[1]]
+    else:
+        print('Path \'{}\' is neither a file nor a directory.'.format(os.path.abspath(path)))
+        return
+
+    # If path is a directory, iterate over the contained files and directories.
+    # If path is a file, just 'iterate' over this single file.
+    for file_name in file_names:
+        file_path = os.path.join(path, file_name)
+        # If recursive, call the process method on any subdirectories
+        if recursive and os.path.isdir(file_path):
+            process_file_or_directory(file_path, True)
+        # Create preview thumbnails of (potential) video files
+        elif os.path.isfile(file_path) and has_video_extension(file_name):
+            create_preview_thumbnails(file_path)
+
 def parse_args() -> Namespace:
     parser = ArgumentParser(description='Pyhton Video Thumbnailer. Command line tool for creating video preview thumbnails.',
                             formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--recursive',
+                         action='store_true',
+                         help='If creating preview thumbnails of video files in a directory, process subdirectories recursively.')
     parser.add_argument('filename',
                          nargs='?',
                          type=str,
@@ -45,3 +95,4 @@ def parse_args() -> Namespace:
 
 if __name__ == '__main__':
     args = parse_args()
+    process_file_or_directory(args.filename, args.recursive)
